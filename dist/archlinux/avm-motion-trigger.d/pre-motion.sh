e09dd0aee14402a23df1efd84df25e0fe20e4a7e
Hermann Mayer <jack@hermann-mayer.net>
2015-10-25 01:20:17 +0200

    [Dist] Added sample custom hooks and updated the sample configurations. (See #16)

---
+++ dist/archlinux/avm-motion-trigger.d/pre-motion.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# avm-motion-trigger - custom hook
+#
+# Custom hook for the `motion detected` action
+#
+# Keep in mind that this script is executed in a non-blocking manner. So the
+# daemon will not wait for this script to finish.
